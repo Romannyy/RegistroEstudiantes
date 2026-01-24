7809f19e0185dc611843198d15138a3ceece4405
Romanny Hernandez <romanny_hernandez@ucne.edu.do>
2026-01-24 06:50:04 -0400

    Más detalle en las validaciones.

--- RegistroAsignaturas/Models/Asignaturas.cs
+++ RegistroAsignaturas/Models/Asignaturas.cs
@@ -7,16 +7,17 @@ namespace RegistroAsignaturas.Models
         [Key]
         public int AsignaturaId { get; set; }
 
-        [Required(ErrorMessage = "Campo obligatorio.")]
+        [Required(ErrorMessage = "Nombre de asignatura obligatorio.")]
         public string Nombre { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Campo obligatorio.")]
-        public int Creditos { get; set; }   
+        [Required(ErrorMessage = "Créditos obligatorios.")]
+        [Range(1, 10, ErrorMessage = "Los créditos deben estar entre 1 y 10.")]
+        public int Creditos { get; set; } = 1;
 
-        [Required(ErrorMessage = "Campo obligatorio.")]
+        [Required(ErrorMessage = "Código de asignatura obligatorio.")]
         public string Codigo { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Campo obligatorio.")]
+        [Required(ErrorMessage = "La asignatura debe tener un aula.")]
         public string Aula { get; set; } = string.Empty;
        
     }
